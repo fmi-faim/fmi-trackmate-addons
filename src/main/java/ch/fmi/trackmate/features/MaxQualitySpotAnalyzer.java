385eab4878b1c40e210e4bd6d94fdb1548393c8a
Jean-Yves Tinevez <tinevez@pasteur.fr>
2021-08-25 10:19:17 +0200

    Fix the MaxQualitySpotAnalyzer issues with TrackMate v7.
    The SpotAnalyzer interface drastically changed and we do not ask
    the instances to store the model anymore, but pass the spots to
    compute directly. So we look for the max quality in frame in
    another way.

--- src/main/java/ch/fmi/trackmate/features/MaxQualitySpotAnalyzer.java
+++ src/main/java/ch/fmi/trackmate/features/MaxQualitySpotAnalyzer.java
@@ -1,65 +1,52 @@
 package ch.fmi.trackmate.features;
 
-import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
 
-import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.Spot;
-import fiji.plugin.trackmate.SpotCollection;
 import fiji.plugin.trackmate.features.spot.SpotAnalyzer;
 
 public class MaxQualitySpotAnalyzer<T> implements SpotAnalyzer<T> {
 
-	private final Model model;
-	private final int frame;
-	private String errorMessage;
-	private long processingTime;
-
-	public MaxQualitySpotAnalyzer(final Model model, final int frame) {
-		this.model = model;
-		this.frame = frame;
-	}
-
 	@Override
-	public boolean checkInput() {
-		return true;
-	}
-
-	@Override
-	public String getErrorMessage() {
-		return errorMessage;
-	}
-
-	@Override
-	public boolean process() {
-		final SpotCollection sc = model.getSpots();
+	public void process( final Iterable< Spot > spotIt ) {
+		/*
+		 * Map that stores the spots with the highest quality in a frame as
+		 * values, with the frames of these spots as keys.
+		 */
+		final Map< Integer, Spot > maxQualitySpots = new HashMap<>();
 
 		/*
-		 * First iteration to get max quality
+		 * First iteration to get spot with max quality in every frame.
 		 */
-		Iterator<Spot> spotIt = sc.iterator(frame, false);
-		double max = Double.NEGATIVE_INFINITY;
-		while (spotIt.hasNext()) {
-			final double quality = spotIt.next().getFeature(Spot.QUALITY);
-			if (quality > max)
-				max = quality;
+
+		for ( final Spot spot : spotIt ) {
+			final Integer frame = Integer.valueOf( spot.getFeature( Spot.FRAME ).intValue() );
+			final Spot spotMax = maxQualitySpots.get( frame );
+
+			if ( spotMax == null ) {
+				// We don't have a spot for this frame yet. Initialize then loop.
+				maxQualitySpots.put( frame, spot );
+				continue;
+			}
+			
+			// Is the current spot having a larger quality? If yes we store it.
+			final double qualityMax = spotMax.getFeature( Spot.QUALITY ).doubleValue();
+			final double quality = spot.getFeature( Spot.QUALITY ).doubleValue();
+			if ( quality > qualityMax )
+				maxQualitySpots.put( frame, spot );
 		}
 
 		/*
 		 * Second iteration to label spot with max quality
 		 */
-		spotIt = sc.iterator(frame, false);
-		while (spotIt.hasNext()) {
-			Spot spot = spotIt.next();
-			spot.putFeature(MaxQualitySpotAnalyzerFactory.HAS_MAX_QUALITY_IN_FRAME,
-					spot.getFeature(Spot.QUALITY) < max ? Double.valueOf(0) : Double.valueOf(1));
-		}
 
-		return true;
+		// All spots get the FALSE label first.
+		final Double falseVal = Double.valueOf( 0. );
+		spotIt.forEach( s -> s.putFeature( MaxQualitySpotAnalyzerFactory.HAS_MAX_QUALITY_IN_FRAME, falseVal ) );
+
+		// The spot we flagged in 1st iteration get labeled with TRUE.
+		final Double trueVal = Double.valueOf( 1. );
+		maxQualitySpots.values().forEach( s -> s.putFeature( MaxQualitySpotAnalyzerFactory.HAS_MAX_QUALITY_IN_FRAME, trueVal ) );
 	}
-
-	@Override
-	public long getProcessingTime() {
-		return processingTime;
-	}
-
 }
